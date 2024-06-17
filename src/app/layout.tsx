6d083f3dc8b440234d6509520cd5802eab52ddf8
arnabw <arnabsahawrk@gmail.com>
2024-06-17 13:56:28 +0600

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Honest Opinion",
+    default: "Honest Opinion",
+  },
+  description: "Honest Opinion - Where Your Opinion Stays Anonymous",
+  keywords: ["Honest Opinion", "Secret Message", "Anonymous Message"],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
