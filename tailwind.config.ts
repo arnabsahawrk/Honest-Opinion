399903d3dd9c98cb9b887ee77de27b32d24465c3
arnabw <arnabsahawrk@gmail.com>
2024-06-17 18:29:41 +0600

    established database connection

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        bgPrimary: "#444444",
+        bgSecondary: "#DA0037",
+        textPrimary: "#EDEDED",
+        textSecondary: "#171717",
       },
     },
   },
