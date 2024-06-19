3a91f2f4dabb94844286e0a77813c37950cf635f
arnabw <arnabsahawrk@gmail.com>
2024-06-19 16:42:03 +0600

    added sign up page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,5 @@
-"use client";
-import { useSession, signIn, signOut } from "next-auth/react";
+const SignIn = () => {
+  return <div>sign in page</div>;
+};
 
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  );
-}
+export default SignIn;
